4d5e4c69549ac3556c0f15fd7152a6847a9d47d8
Ardogar <Alexgranovski@gmail.com>
2019-01-06 15:04:01 +0300

    1

---
+++ gulp/tasks/img_min.js
@@ -0,0 +1,16 @@
+'use strict'
+
+module.exports = function() {
+  $.gulp.task("img", function() {
+    return $.gulp.src([
+      "./src/img/**/*.{png,jpg,svg}",
+      "!./src/img/**/sprite.svg",
+      "!./src/img/**/icon-*.svg"])
+      .pipe($.gp.imagemin([
+        $.gp.imagemin.optipng({optimizationLevel: 3}),
+        $.gp.imagemin.jpegtran({progressive: true}),
+        $.gp.imagemin.svgo()
+      ]))
+      .pipe($.gulp.dest("./build/img"));
+  });
+}
---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+'use strict'
+
+global.$ = {
+  path: {
+    task: require('./gulp/paths/tasks.js')
+  },
+  gulp: require('gulp'),
+  del: require('del'),
+  autoprefixer: require('autoprefixer'),
+  htmlInclude: require('posthtml-include'),
+  nodeNormalize: require('node-normalize-scss'),
+  browserSync: require('browser-sync').create(),
+  posthtmlInclude: require('posthtml-include'),
+  gp: require('gulp-load-plugins') ()
+};
+
+$.path.task.forEach(function(taskPath) {
+  require(taskPath) ();
+});
+
+$.gulp.task('reBuild', $.gulp.series(
+  'clean',
+  $.gulp.parallel(
+    'img',
+    'imgWEBP',
+    'sprite'
+  ),
+  $.gulp.parallel(
+    'html',
+    'scss',
+    'js',
+    'copy'
+  )
+));
+$.gulp.task('serve', $.gulp.parallel(
+  'serv',
+  'watch'
+));
+$.gulp.task('build', $.gulp.series(
+  'clean',
+  $.gulp.parallel(
+    'img',
+    'imgWEBP',
+    'sprite'
+  ),
+  $.gulp.parallel(
+    'html',
+    'scss',
+    'js',
+    'copy'
+  ),
+  $.gulp.parallel(
+    'serv',
+    'watch'
+  )
+))
